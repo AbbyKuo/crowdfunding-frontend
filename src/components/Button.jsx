a30cce2c2b87380821270774ccb54f8c06e09a6c
AbbyKuo <abbykuo8776@gmail.com>
2024-11-26 18:53:21 +1000

    Created Button.jsx and Buttom.css in components folder

---
+++ src/components/Button.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Button.css';
+
+const Button = ({
+    label = "Click me",    // Default text
+    onClick,              // Function to execute on click
+    type = "button",      // Default type
+    variant = "primary",  // 'primary' or 'secondary'
+    disabled = false,     // Disabled state
+}) => {
+    const buttonClass = disabled
+        ? "button button-disabled"
+        : `button button-${variant}`;
+
+    return (
+        <button
+            type={type}
+            className={buttonClass}
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {label}
+        </button>
+    );
+};
+
+export default Button;
+
