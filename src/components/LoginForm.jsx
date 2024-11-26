3c85150c5ca01a5da3480bd63f96d2e31b9b341d
AbbyKuo <abbykuo8776@gmail.com>
2024-11-26 20:28:20 +1000

    Called the post-login function when LoginForm is submitted

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import postLogin from "../api/post-login.js";
 
 function LoginForm() {
     const [credentials, setCredentials] = useState({
@@ -6,13 +7,25 @@ function LoginForm() {
             password: "",
         });
         
-        const handleChange = (event) => {
-            const { id, value } = event.target;
-            setCredentials((prevCredentials) => ({
-                ...prevCredentials,
-                [id]: value,
-            }));
-        };
+    const handleChange = (event) => {
+        const { id, value } = event.target;
+        setCredentials((prevCredentials) => ({
+            ...prevCredentials,
+            [id]: value,
+        }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (credentials.username && credentials.password) {
+            postLogin(
+                credentials.username,
+                credentials.password
+            ).then((response) => {
+                console.log(response);
+            });
+        }
+    };
 
     return (
         <form>
@@ -34,7 +47,9 @@ function LoginForm() {
                 onChange={handleChange}
             />
         </div>
-        <button type="submit">Login</button>
+        <button type="submit" onClick={handleSubmit}>
+            Login
+        </button>
         </form>
     );
 }
