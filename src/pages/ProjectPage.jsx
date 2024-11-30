5de93c2f4bb0471cfd797bed83751dfd4bd32b74
AbbyKuo <abbykuo8776@gmail.com>
2024-11-30 14:26:14 +1000

    Created ProjectPage.css and modified the layout on ProjectPage.jsx

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,41 +1,48 @@
 import { useParams } from "react-router-dom";
 import useProject from "../hooks/use-project";
 import Footer from '../components/Footer';
+import "./ProjectPage.css";
 
 function ProjectPage() {
-    // Here we use a hook that comes for free in react router called `useParams` to get the id from the URL so that we can pass it to our useProject hook.
     const { id } = useParams();
-    // useProject returns three pieces of info, so we need to grab them all here
-    const { project, isLoading, error } = useProject(id);    
+    const { project, isLoading, error } = useProject(id);
 
     if (isLoading) {
-            return (<p>loading...</p>)
-        }
-            
-        if (error) {
-            return (<p>{error.message}</p>)
-        }
+        return (<p>Loading...</p>);
+    }
+
+    if (error) {
+        return (<p>{error.message}</p>);
+    }
 
     return (
         <div>
-            <main>
-                <h2>{project.title}</h2>
-                <h3>Created at: {project.date_created}</h3>
-                <h3>{`Status: ${project.is_open}`}</h3>
-                <h3>Pledges:</h3>
-                <ul>
-                    {project.pledges.map((pledgeData, key) => {
-                        return (
+            <div className="project-page-container">
+                {/* Left Column: Project Details */}
+                <div className="project-details">
+                    <h2>{project.title}</h2>
+                    <h3>Created at: {project.date_created}</h3>
+                    <h3>{`Status: ${project.is_open ? 'Open' : 'Closed'}`}</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Vivamus luctus eros aliquet convallis ultricies.
+                    </p>
+                </div>
+                {/* Right Column: Pledges */}
+                <div className="project-pledges">
+                    <h3>Pledges</h3>
+                    <ul>
+                        {project.pledges.map((pledgeData, key) => (
                             <li key={key}>
-                                {pledgeData.amount} from {pledgeData.supporter}
+                                ${pledgeData.amount} from {pledgeData.supporter}
                             </li>
-                        );
-                    })}
-                </ul>
-            </main>
+                        ))}
+                    </ul>
+                </div>
+            </div>
             <Footer />
         </div>
-    )
+    );
 }
 
-export default ProjectPage
+export default ProjectPage;
