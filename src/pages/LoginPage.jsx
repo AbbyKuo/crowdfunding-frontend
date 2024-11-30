06df1b4c6d82a13d71403dda2faf5ef9cdea708b
AbbyKuo <abbykuo8776@gmail.com>
2024-11-30 14:04:14 +1000

    Created LoginPage.css and added hero component to LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,27 @@
 import LoginForm from "../components/LoginForm";
-import Footer from '../components/Footer';
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import "./LoginPage.css";
 
 function LoginPage() {
     return (
         <div>
-            <LoginForm />
-                <Footer />
+            <div className="login-page-container">
+                <Hero 
+                    title="Hello"
+                    subtitle="Welcome Back!"
+                    image=""
+                    buttonLabel=""
+                    buttonLink=""
+                />
+                <div className="login-form-wrapper">
+                    <div className="login-form-container">
+                        <h2>Log In</h2>
+                        <LoginForm />
+                    </div>
+                </div>
+            </div>
+            <Footer />
         </div>
     );
 }
