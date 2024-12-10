be603de4659dfd5b3df35fed82aa9dedca51a127
AbbyKuo <abbykuo8776@gmail.com>
2024-12-10 19:20:20 +1000

    created use-auth hook and use the hook to update state in LoginForm.jsx, updated the NavBar.jsx component to change the button for login when a user is logged in

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import postLogin from "../api/post-login.js";
 import "./LoginForm.css";
+import useAuth from "../hooks/use-auth.js";
 
 function LoginForm() {
     const navigate = useNavigate(); 
+    const {auth, setAuth} = useAuth();
     
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
@@ -34,6 +36,9 @@ function LoginForm() {
         postLogin(credentials.username, credentials.password)
             .then((response) => {
                 window.localStorage.setItem("token", response.token);
+                setAuth({
+                    token: response.token,
+                });
                 navigate(location.state?.from || "/"); // Redirect to previous page or home
             })
             .catch(()=> {
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,31 @@
 import { Link, Outlet } from "react-router-dom";
 import "./NavBar.css";
+import useAuth from "../hooks/use-auth.js";
 
 function NavBar() {
+    const {auth, setAuth} = useAuth();
+
+    const handleLogout = () => {
+        window.localStorage.removeItem("token");
+        setAuth({ token: null });
+    };
+
+    console.log(auth)
+
     return (
         <div>
             <nav className="navbar-container">
                 <Link to="/">Home</Link>
                 <Link to="/about">About Us</Link>
                 <Link to="/contact">Contact Us</Link>
-                <Link to="/login">Log In</Link>
+                {/* <Link to="/login">Log In</Link> */}
+                {auth.token ? (
+                    <Link to="/" onClick={handleLogout}>
+                        Log Out
+                    </Link>
+                    ) : (
+                    <Link to="/login">Login</Link>
+                )}
             </nav>
         </div>
     );
